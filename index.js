08550602565a4fd63f8956ff74927fd73a2f2b37
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2024-06-14 16:10:02 -0400

    excluir Usuário

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const conn = require("./db/conn");
 
 const Usuario = require("./models/Usuario");
 const Jogo = require("./models/Jogo");
+
+const handlebars = require ("express-handlebars")
 const express = require("express");
+
 const app = express();
+
+app.engine("handlebars", handlebars.engine())
+app.set("view engine", "handlebars")
+
 
 conn
 .sync()
@@ -24,11 +31,20 @@ app.use(
 
 
 app.get("/usuarios/novo", (req, res) => {
-    res.sendFile(`${__dirname}/views/formUsuario.html`)
+    res.render(`formUsuario`)
 })
 
 app.get("/jogos/novo", (req, res) => {
-    res.sendFile(`${__dirname}/views/formJogo.html`)
+    res.render(`formJogo`)
+})
+
+app.get("/", (req, res) => {
+    res.render(`home`)
+})
+
+app.get("/usuarios", async(req, res) => {
+    const usuarios = await Usuario.findAll({raw: true})
+    res.render(`usuarios`, {usuarios})
 })
 
 app.post("/usuarios/novo", async (req, res) =>{
@@ -44,6 +60,7 @@ app.post("/usuarios/novo", async (req, res) =>{
 
     res.send("Usuário inserido sob id " + usuario.id)
 });
+
 
 app.post("/jogos/novo", async (req, res) =>{
     const titulo = req.body.titulo;
@@ -61,6 +78,45 @@ app.post("/jogos/novo", async (req, res) =>{
     res.send("Jogo inserido sob id " + jogo.id)
 });
 
+app.get("/usuarios/:id/atualizar", async (req, res)=>{
+    const id = req.params.id
+    const usuario = await Usuario.findByPk(id, { raw: true})
+
+    res.render("formUsuario", {usuario})
+})
+
+app.post("/usuarios/:id/atualizar", async (req, res)=>{
+    const id = req.params.id
+
+    const dadosUsuario = {
+        nickname: req.body.nickname,
+        nome: req.body.nome,
+    }
+
+    const registroAfetados = await Usuario.update(dadosUsuario, {where:{id: id}})
+
+    if (registroAfetados > 0) {
+        res.redirect("/usuarios")
+    }
+    else{
+        res.send("Erro ao atualizar usuário!!!!!")
+    }
+    
+})
+
+app.post("/usuarios/excluir", async (req, res) =>{
+
+    const id = req.body.id
+
+    const registroAfetados = await Usuario.destroy({where:{id: id}})
+
+    if (registroAfetados > 0) {
+        res.redirect("/usuarios")
+    }
+    else{
+        res.send("Erro excluir usuário!!!!!")
+    }
+})
 
 app.listen(8000, () => {
     console.log("server rodando na porta 8000!")
